a68bc1d49113dd1d0d2c66087582410c11e6d86e
bobbylemm <chidozienwoga@gmail.com>
2025-11-27 21:50:46 +0100

    feat: add page for docs

---
+++ frontend/next.config.mjs
@@ -0,0 +1,40 @@
+import nextMDX from '@next/mdx'
+
+import { recmaPlugins } from './src/mdx/recma.mjs'
+import { rehypePlugins } from './src/mdx/rehype.mjs'
+import { remarkPlugins } from './src/mdx/remark.mjs'
+import withSearch from './src/mdx/search.mjs'
+
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+})
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  outputFileTracingIncludes: {
+    '/**/*': ['./src/app/**/*.mdx'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:8000/:path*',
+      },
+      {
+        source: '/docs',
+        destination: 'http://127.0.0.1:8000/docs',
+      },
+      {
+        source: '/openapi.json',
+        destination: 'http://127.0.0.1:8000/openapi.json',
+      },
+    ]
+  },
+}
+
+export default withSearch(withMDX(nextConfig))
